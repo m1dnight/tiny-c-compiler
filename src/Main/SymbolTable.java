ef4ca0736068f02fb0e35941b095360b1f80d510
Christophe De Troyer <christophe.detroyer@gmail.com>
2014-03-31 15:02:01 +0200

    symboltable werkt, alles compiled

--- src/Main/SymbolTable.java
+++ src/Main/SymbolTable.java
@@ -12,22 +12,38 @@ public class SymbolTable {
     // The list that will hold all the identifiers in this scope
     ArrayList<Object> symbolList = new ArrayList<Object>();
 
+    private int level = 0;
+
 
     public SymbolTable()
     {
         this.parent = null;
     }
 
+    public SymbolTable(int level)
+    {
+        this.parent = null;
+        this.level = level;
+    }
+
     public SymbolTable NewScope(String functionName)
     {
+        // Create a new scope and add the functioname
+        // as the label of the scope
+        //TODO Anything else..?
+
         SymbolTable newScope = new SymbolTable();
         newScope.function = functionName;
         newScope.parent = this;
         return newScope;
     }
 
-    public void Insert(String functionName, Object typeInformation)
+    public void Insert(String functionName, TypeInfo typeInformation)
     {
+        if(typeInformation != null)
+            System.out.printf("Inserted %6s with type %10s in current scope.\n", functionName, typeInformation.type.name());
+        else
+            System.out.printf("Inserted %10s in current scope.\n", functionName);
         symbolList.add(functionName);
     }
 }
