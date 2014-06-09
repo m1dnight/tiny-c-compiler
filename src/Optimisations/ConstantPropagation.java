9db9f1d263a90b62b856260133fac55047a8141f
Christophe De Troyer <christophe.detroyer@gmail.com>
2014-06-09 20:56:02 +0200

    before try in eclipse

--- src/Optimisations/ConstantPropagation.java
+++ src/Optimisations/ConstantPropagation.java
@@ -1,9 +1,21 @@
 package Optimisations;
 
+import Assembly.BasicBlock;
+import CodeGeneration.ThreeAddressCode;
+
+import java.util.ArrayList;
+
 /**
  * Created by christophe on 09.06.14.
  */
 public class ConstantPropagation
 {
-    
+    public BasicBlock Optimize(BasicBlock block)
+    {
+        ArrayList<ThreeAddressCode> newTacs = new ArrayList<ThreeAddressCode>();
+        for(ThreeAddressCode tac : block.getTacs())
+        {
+
+        }
+    }
 }
